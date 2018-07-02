2e9e7c521860c4cb07fea31efe13da94dbb6b0d0
K.Kimura <dotnsf@gmail.com>
2018-07-02 15:40:37 +0900

    NLC settings added in settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,18 @@ exports.app_port = 3000;
 exports.app_theme = 'default';
 exports.search_analyzer = 'japanese';
 exports.superSecret = 'welovebluecms';
+exports.nlc_username = '';
+exports.nlc_password = '';
+exports.nlc_name = 'bluecms';
 
 if( process.env.VCAP_SERVICES ){
   var VCAP_SERVICES = JSON.parse( process.env.VCAP_SERVICES );
   if( VCAP_SERVICES && VCAP_SERVICES.cloudantNoSQLDB ){
     exports.cloudant_username = VCAP_SERVICES.cloudantNoSQLDB[0].credentials.username;
     exports.cloudant_password = VCAP_SERVICES.cloudantNoSQLDB[0].credentials.password;
+  }
+  if( VCAP_SERVICES && VCAP_SERVICES.natural_language_classifier ){
+    exports.nlc_username = VCAP_SERVICES.natural_language_classifier[0].credentials.username;
+    exports.nlc_password = VCAP_SERVICES.natural_language_classifier[0].credentials.password;
   }
 }
