f278cc767357fb47214ed70cc41aa8367044d60f
K.Kimura <dotnsf@jp.ibm.com>
2018-07-03 21:19:12 +0900

    add parameter in settings.js

--- settings.js
+++ settings.js
@@ -9,6 +9,7 @@ exports.nlc_username = '';
 exports.nlc_password = '';
 exports.nlc_name = 'bluecms';
 exports.nlc_language = 'ja';
+exports.nlc_url = '';
 
 if( process.env.VCAP_SERVICES ){
   var VCAP_SERVICES = JSON.parse( process.env.VCAP_SERVICES );
@@ -19,5 +20,6 @@ if( process.env.VCAP_SERVICES ){
   if( VCAP_SERVICES && VCAP_SERVICES.natural_language_classifier ){
     exports.nlc_username = VCAP_SERVICES.natural_language_classifier[0].credentials.username;
     exports.nlc_password = VCAP_SERVICES.natural_language_classifier[0].credentials.password;
+    exports.nlc_url = VCAP_SERVICES.natural_language_classifier[0].credentials.url;
   }
 }
